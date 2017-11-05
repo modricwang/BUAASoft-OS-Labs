4449d6615df9e46d0bda6adb9c8db35f5e81e7e1
Modric Wang <modricwang@126.com>
2017-11-05 21:24:31 +0800

    update lab4

--- Lab4/data_gen.c
+++ Lab4/data_gen.c
@@ -3,14 +3,14 @@
 #include <time.h>
 
 int main() {
-	freopen("data.txt","w",stdout);
+	freopen("data.txt", "w", stdout);
 	srand(time(0));
-	const int lines=5000;
-	const int lenth_per_line=20;
-	for (int i=0; i<lines; i++) {
-		for (int j=0;j<lenth_per_line;j++) {
-			printf("%c",(char)(rand()%26+'a'));
-		}	
+	const int lines = 5000;
+	const int lenth_per_line = 20;
+	for (int i = 0; i < lines; i++) {
+		for (int j = 0; j < lenth_per_line; j++) {
+			printf("%c", (char) (rand()%26 + 'a'));
+		}
 		printf("\n");
 	}
 }
--- Lab4/my_mmap.c
+++ Lab4/my_mmap.c
@@ -8,40 +8,40 @@
 #include <time.h>
 
 void data_gen() {
-    FILE *f = fopen("in.txt", "w");
-    srand(time(0));
-    const int lines = 5000;
-    const int lenth_per_line = 20;
-    for (int i = 0; i < lines; i++) {
-        for (int j = 0; j < lenth_per_line; j++) {
-            fprintf(f, "%c", (char) (rand() % 26 + 'a'));
-        }
-        fprintf(f, "\n");
-    }
-    fclose(f);
+	FILE *f = fopen("in.txt", "w");
+	srand(time(0));
+	const int lines = 5000;
+	const int lenth_per_line = 20;
+	for (int i = 0; i < lines; i++) {
+		for (int j = 0; j < lenth_per_line; j++) {
+			fprintf(f, "%c", (char) (rand()%26 + 'a'));
+		}
+		fprintf(f, "\n");
+	}
+	fclose(f);
 }
 
 int main() {
-    data_gen();
-    int file_in = open("in.txt", O_RDONLY);
-    int file_out = open("out.txt", O_RDWR | O_CREAT, ~0);
+	data_gen();
+	int file_in = open("in.txt", O_RDONLY);
+	int file_out = open("out.txt", O_RDWR | O_CREAT, ~0);
 
-    struct stat stat_file_in;
-    fstat(file_in, &stat_file_in);
+	struct stat stat_file_in;
+	fstat(file_in, &stat_file_in);
 
-    lseek(file_out, stat_file_in.st_size - 1, 0);
-    write(file_out, "", 1);
+	lseek(file_out, stat_file_in.st_size - 1, 0);
+	write(file_out, "", 1);
 
-    void *src = mmap(NULL, stat_file_in.st_size, PROT_READ, MAP_SHARED, file_in, 0);
-    void *dst = mmap(NULL, stat_file_in.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, file_out, 0);
+	void *src = mmap(NULL, stat_file_in.st_size, PROT_READ, MAP_SHARED, file_in, 0);
+	void *dst = mmap(NULL, stat_file_in.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, file_out, 0);
 
-    memcpy(dst, src, stat_file_in.st_size);
+	memcpy(dst, src, stat_file_in.st_size);
 
-    munmap(src, stat_file_in.st_size);
-    munmap(dst, stat_file_in.st_size);
+	munmap(src, stat_file_in.st_size);
+	munmap(dst, stat_file_in.st_size);
 
-    close(file_in);
-    close(file_out);
+	close(file_in);
+	close(file_out);
 
-    return 0;
+	return 0;
 }
